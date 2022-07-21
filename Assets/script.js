b6db80a1b7c6f1986ae84511b1c363cca35c568a
Vader <gassanb97@gmail.com>
2022-07-21 08:01:59 -0700

    finished most of JS and README

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,120 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Arrays of character options allowed
+special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "`", "{", "|", "}", "~", "]"];
+
+numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "O", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variables
+var option;
+
+var userChoice;
+
+var allowSpecial;
+
+var allowNumeric;
+
+var allowLowercase;
+
+var allowUppercase;
+ 
+// The function used to generate password
+function generatePassword() {
+
+    // Prompting user to insert character choice
+    userChoice = parseInt(prompt("How many characters would you like your password to contain? You must choose between 8 and 128 characters"));
+  
+    if (!userChoice) {
+        alert("Please insert a valid value to proceed");
+    } else if (userChoice < 8 || userChoice > 128) {
+      
+        userChoice = parseInt(prompt("You must choose between 8 and 128 characters!"));
+
+    // Confirmation alerts requiring user to choose if they want a specific character or not   
+    } else {
+
+        // Confirming addition of numeric characters
+        allowNumeric = confirm("Click OK to confirm including numeric characters");
+
+        // Confirming addition of special characters
+        allowSpecial = confirm("Click OK to confirm including special characters");
+
+        // Confirming addition of uppercase characters
+        allowUppercase = confirm("Click OK to confirm including uppercase characters");
+
+        // Confirming addition of lowercase characters
+        allowLowercase = confirm("Click OK to confirm including lowercase characters");
+    };
+ 
+    // Alert if user does not input correct key/amount
+    if (!allowSpecial && !allowNumeric && !allowUppercase && !allowLowercase) {
+        option = alert("You must choose at least one of the criteria!");
+
+        // else if statements for different user responses to alerts
+        } else if (allowSpecial && allowNumeric && allowUppercase && allowLowercase) {
+        option = special.concat(numeric, lowercase, uppercase);
+
+        } else if (allowSpecial && allowNumeric && allowUppercase) {
+        option = special.concat(numeric, uppercase);
+
+        } else if (allowSpecial && allowNumeric && allowLowercase) {
+        option = special.concat(numeric, lowercase);
+
+        } else if (allowSpecial && allowLowercase && allowUppercase) {
+        option = special.concat(lowercase, uppercase);
+
+        } else if (allowNumeric && allowLowercase && allowUppercase) {
+        option = numeric.concat(lowercase, uppercase);
+
+        } else if (allowSpecial && allowNumeric) {
+        option = special.concat(numeric);
+
+        } else if (allowSpecial && allowLowercase) {
+        option = special.concat(lowercase);
+
+        } else if (allowSpecial && allowUppercase) {
+        option = special.concat(uppercase);
+
+        } else if (allowLowercase && allowNumeric) {
+        option = lowercase.concat(numeric);
+
+        } else if (allowLowercase && allowUppercase) {
+        option = lowercase.concat(uppercase);
+
+        } else if (allowNumeric && allowUppercase) {
+        option = numeric.concat(uppercase);
+
+        } else if (allowSpecial) {
+        option = special;
+
+        } else if (allowNumeric) {
+        option = numeric;
+
+        } else if (allowLowercase) {
+        option = lowercase;
+
+        } else if (allowUppercase) {
+        option = uppercase;
+    };
+
+    var password = [];
+
+    // Generating a random output of all characters allowed
+    for (var i = 0; i < userChoice; i++) {
+        var confirmOp = option[Math.floor(Math.random() * option.length)];
+        password.push(confirmOp);
+    }
+  
+    return password;
+}
+
+// Generating password in the 'your secure password' box
+generateBtn.addEventListener("click", writePassword);
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +123,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generatePassword();
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Password Generator
+# Password Generator 🔒
 
+Welcome! ☺👋
+
+## Description 📝
+
+This is a password generator simply created to generate randomized passwords between 8-128 characters in length including special characters, numerical characters, uppercase letters, and lowercase letters. This simplifies creating secure passwords for your online accounts rather than making passwords that may be unsecure. In creating this generator I have learned a lot about 'if' and 'else' statements and how they work to create a responsive webpage.
+
+## Deployed Webpage 💻
+
+## Visuals 📷
+
+## Credits 🌎
+
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat
+https://www.w3schools.com/js/js_functions.asp
+https://stackoverflow.com/
+
+Chris Shoe 🥇
+
+## License 🔑
+
+MIT License
+
+Copyright (c) [2022] [Gassan-Bundakji]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
